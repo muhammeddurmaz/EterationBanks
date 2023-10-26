e1b5c22279cef4621202e92d1398744cea83ea94
muhammeddurmaz <mdurmaz.dev@gmail.com>
2023-10-26 18:35:41 +0300

    start project

---
+++ src/main/java/com/muhammed/eterationbanks/EterationBanksApplication.java
@@ -0,0 +1,13 @@
+package com.muhammed.eterationbanks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EterationBanksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EterationBanksApplication.class, args);
+    }
+
+}
