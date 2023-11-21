cab9afa73fe0e9ba399176f8a97fb753edd9e08f
muhammeddurmaz <mdurmaz.dev@gmail.com>
2023-11-21 12:41:19 +0300

    exceptions added

---
+++ src/main/java/com/muhammed/eterationbank/exception/AccountNotFound.java
@@ -0,0 +1,8 @@
+package com.muhammed.eterationbank.exception;
+
+public class AccountNotFound extends RuntimeException{
+
+    public AccountNotFound(String message) {
+        super(message);
+    }
+}
