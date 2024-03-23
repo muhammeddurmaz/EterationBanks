a45ceca250587ff9d7df4bef61185bd5bc34eb00
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-03-23 13:16:01 +0300

    bugfix

--- src/test/java/com/muhammed/eterationbank/EterationBankApplicationTests.java
+++ src/test/java/com/muhammed/eterationbank/EterationBankApplicationTests.java
@@ -1,10 +1,10 @@
-package com.muhammed.eterationbanks;
+package com.muhammed.eterationbank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EterationBanksApplicationTests {
+class EterationBankApplicationTests {
 
     @Test
     void contextLoads() {
